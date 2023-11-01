9c34d4c083e6c469ac26faf239dbdbe9c7632622
Arya Parvizi <aryaparvizi98@gmail.com>
2023-11-01 17:58:56 -0300

    Add FPSMovement.cs

---
+++ HCI Target Gallery/Assets/Scripts/FPSMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float walkspeed = 10f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+    }
+}
