4336677a1f7800675b386e19ca7d0315166a46c1
Arya Parvizi <aryaparvizi98@gmail.com>
2023-11-01 18:23:22 -0300

    Update FPSMovement.cs, added the movement.

--- HCI Target Gallery/Assets/Scripts/FPSMovement.cs
+++ HCI Target Gallery/Assets/Scripts/FPSMovement.cs
@@ -27,5 +27,10 @@ public class FPSMovement : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move *walkspeed * Time.deltaTime);
     }
 }
