386f0310d1469c15af8a59800c1a63b3a9326045
Arya Parvizi <aryaparvizi98@gmail.com>
2023-11-02 21:31:04 -0300

    Feat: added mouse aim feature.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float sensitivity;
+    public float sensitivity = 200f;
     public Transform player;
 
     float xRotation = 0f;
     void Start()
     {
-        Cursor.lockState = CursorLockMode.locked;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mosueX = Input.GetAxis("Mouse X") = sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") = sensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
         
     }
 }
