fccb6b9d95ed51de0874aea01dc6811f7b494c84
Arya Parvizi <aryaparvizi98@gmail.com>
2023-11-21 09:32:26 -0400

    feat: Target spawn, now is spawning on a circular arc rather than a straight line, now targets rotate to the player to counter the parallex effect.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {   
+    public Transform player;
     public bool isTargetPractice;
+    public float minHeight;
+    public float maxHeight;
+    public float arcSize;
+    public float radius;
     public float health = 10f;
     public float defaultHealth;    
     // Start is called before the first frame update
     void Start()
     {
         defaultHealth = health;
+        float height = Random.Range(minHeight, maxHeight);
+        float theta = Random.Range(0, Mathf.Deg2Rad*arcSize);
+        transform.position = new Vector3(radius*Mathf.Sin(-theta/2)+player.position.x, height, radius*Mathf.Cos(theta/2)+player.position.z);
+        transform.LookAt(player);
+        transform.forward *=-1;
     }
 
     // Update is called once per frame
@@ -24,7 +34,11 @@ public class Target : MonoBehaviour
     {
         if(isTargetPractice){
             health = defaultHealth;
-            gameObject.transform.position = new Vector3(Random.Range(-8,7), 5, Random.Range(4,10));
+            float height = Random.Range(minHeight, maxHeight);
+            float theta = Random.Range(0, Mathf.Deg2Rad*arcSize);
+            gameObject.transform.position = new Vector3(radius*Mathf.Sin(-theta/2)+player.position.x, height, radius*Mathf.Cos(theta/2)+player.position.z);
+            gameObject.transform.LookAt(player);
+            transform.forward *=-1;
         }
         else{
             Destroy(gameObject);
