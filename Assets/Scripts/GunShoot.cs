fa449681266ae5fd0e0f58f2419d0011672c9c7d
Arya Parvizi <aryaparvizi98@gmail.com>
2023-11-26 13:05:30 -0400

    Add experiment dependant variables.

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -43,17 +43,15 @@ public class GunShoot : MonoBehaviour
             if(Physics.SphereCast(FPSCam.transform.position, assistRadius, FPSCam.transform.forward, out hit, range))
             {
                 Hit(hit);
-                Vector3 distance = hit.transform.position - FPSCam.transform.position;
-                accuracy = distance.magnitude - assistRadius;
-                
-
+                Vector3 distanceVec = hit.transform.position - FPSCam.transform.position;
+                accuracy = distanceVec.magnitude - assistRadius;
             }
         }
         else if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range))
         {
             Hit(hit);
-            Vector3 distance = hit.transform.position - FPSCam.transform.position
-            accuracy = distance.magnitude;
+            Vector3 distanceVec = hit.transform.position - FPSCam.transform.position;
+            accuracy = distanceVec.magnitude;
         }
         else
         {
